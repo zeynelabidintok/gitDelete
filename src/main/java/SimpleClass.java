bed7b4a5067944f6e00c56985f2a0c134bbe4491
zeynel abidin <z.abidintok@gmail.com>
2020-02-06 00:46:11 +0100

    selam ekledik

--- src/main/java/SimpleClass.java
+++ src/main/java/SimpleClass.java
@@ -2,8 +2,9 @@ public class SimpleClass {
     public static void main(String[] args) {
         int a=2, b=0;
         System.out.println(a+b);
+
+
+        System.out.println("selam");
     }
-
-
 
 }
